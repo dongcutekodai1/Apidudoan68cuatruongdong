6c130377a5d43333480cdabb0b6216b84ad13b31
dongcutekodai1 <dongcutekodep199220132013@gmail.com>
2025-12-13 08:59:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const API_URL = 'https://api50-gyw4.onrender.com/history';
 
